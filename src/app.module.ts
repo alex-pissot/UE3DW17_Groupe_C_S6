2ffc1582bcb4a8b3e21c42f1fe0e031e4bbeaed5
Alex Pissot <ylex@hotmail.fr>
2022-01-25 09:41:05 +0000

    import dependance

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGO_URL
+    )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
